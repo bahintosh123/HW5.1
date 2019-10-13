202197b732a46c0d991a46d4d93fb85b2c781e27
bahintosh123 <bahintoshh@gmail.com>
2019-10-13 22:09:55 +0600

    change method2

--- src/com/company/Magic_door.java
+++ src/com/company/Magic_door.java
@@ -1,13 +1,13 @@
 package com.company;
 
 public class Magic_door {
-    public static String[] players = {"n1", "n2", "n3", "n4"};
+    private String[] players = {"n1", "n2", "n3", "n4"};
     private String n1;
     private String n2;
     private String n3;
     private String n4;
 
-    public static String[] getPlayers() {
+    public String[] getPlayers() {
         return players;
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,38 +1,38 @@
 package com.company;
 
-import static com.company.Magic_door.getPlayers;
-import static com.company.Magic_door.players;
 
 public class Main {
 
     public static void main(String[] args) {
-		Hero ahilles = new Hero();
-		ahilles.health = 1000;
-		ahilles.power = 250;
-		ahilles.magic = 50;
-		System.out.println(ahilles.health + " " + ahilles.power + " " + ahilles.magic);
-		Hero hector = new Hero(800, 200, 20);
-		System.out.println(hector.health + " " + hector.power + " " + hector.magic);
-		ahilles.shoutHector();
+        Hero ahilles = new Hero();
+        ahilles.health = 1000;
+        ahilles.power = 250;
+        ahilles.magic = 50;
+        System.out.println(ahilles.health + " " + ahilles.power + " " + ahilles.magic);
+        Hero hector = new Hero(800, 200, 20);
+        System.out.println(hector.health + " " + hector.power + " " + hector.magic);
+        ahilles.shoutHector();
 
-		Boss devil = new Boss();
-		devil.setHealth(1000);
-		devil.setPower(500);
-		devil.setDefense(200);
-		System.out.println(devil.getHealth() + " " + devil.getPower() + " " + devil.getDefense());
-
-
-		Boss devil2 = new Boss();
-		devil.setDefense(300);
-		devil.getHealth();
-		devil.getPower();
-		System.out.println(devil.getDefense());
+        Boss devil = new Boss();
+        devil.setHealth(1000);
+        devil.setPower(500);
+        devil.setDefense(200);
+        System.out.println(devil.getHealth() + " " + devil.getPower() + " " + devil.getDefense());
 
 
-		Magic_door durak = new Magic_door();
-		for (int i = 0; i < players.length; i++) {
-		System.out.println(players[i]);
+        Boss devil2 = new Boss();
+        devil.setDefense(300);
+        devil.getHealth();
+        devil.getPower();
+        System.out.println(devil.getDefense());
 
 
+        Magic_door durak = new Magic_door();
+        String[] players = durak.getPlayers();
+        for (int i = 0; i < players.length; i++) {
+            System.out.println(players[i]);
+
+
+        }
     }
-}}
+}
